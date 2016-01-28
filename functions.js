4330dd716b665becaacf0c6a18af423f7a941a4f
Sonia <sonia.badeau@gmail.com>
2016-01-28 21:56:52 +0000

    functions.js

--- functions.js
+++ functions.js
@@ -44,7 +44,71 @@ var sorted = person.sort(compareLength);
 //This function should take a number and return its square. 
 //Then, use this function with map on an array of numbers to check the result.
 
-var numbers = [4, 9, 16];
-var square = numbers.map(Math.sqrt);
+function returnSquare(number){
+    return number * number;
+}
 
+var numbers = [1, 4, 7, 9, 5];
+var newArray = numbers.map(returnSquare)
 
+var secondArray = numbers.map(function(i){
+    return i + i;
+})
+
+//console.log(secondArray);
+
+//console.log(newArray);
+
+//Create a function that can be used with Array.prototype.map. 
+//This function should be able to take an object and square its “num” property. 
+//Then, use this function with map on an array of objects each containing a “num” property.
+
+var numObj = [
+    {name: "Eight", num: 8},
+    {name: "Five", num: 5},
+    {name: "Ten", num: 10}
+    ];
+function multiply (obj) {
+    return obj.num * obj.num;
+}
+var square = numObj.map(multiply);
+console.log(square);
+// map acts like a loop. It creates a new array 
+
+//Here we are going to do the same but at a higher order. 
+//Create a function called operationMaker that takes only a string called operation as argument. 
+//This string could be “add”, “subtract”, “mult” or “div”. 
+//Your function will return a function that will take two numbers and return the result of running operation on these numbers.
+
+function operationMaker(operation) {
+    switch(operation) {
+  case 'add':
+      return function adder(a,b) {
+          return a + b;
+      }
+      
+  case 'subtract':
+      return function subtracter(a,b) {
+          return a - b;
+      }
+  
+  case 'mult':
+      return function multiplier(a,b) {
+          return a * b;
+      }
+      
+  case 'div':
+      return function divider(a,b) {
+          return a / b;
+      }
+      
+  default:
+      console.log("Bad operation!");
+   };
+   
+}
+
+console.log(operationMaker("add")(5,10));
+console.log(operationMaker("subtract")(5,10));
+console.log(operationMaker("mult")(5,10));
+console.log(operationMaker("div")(5,10));
